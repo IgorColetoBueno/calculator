778c79970bed4a944527c53f2d11bc1f9c694d07
Igor Bueno <igor.bueno@DBC-001272.local>
2023-08-15 11:02:26 -0400

    fix: error treatment

--- frontend/src/store/hooks/index.tsx
+++ frontend/src/store/hooks/index.tsx
@@ -14,92 +14,99 @@ export const useCalculator = () => {
 
   const onDigitSet = useCallback(
     (digit: string) => {
-      const currentCalcIsNan = isNaN(+currentCalc.calc);
+      try {
+        const currentCalcIsNan = isNaN(+currentCalc.calc);
 
-      switch (digit) {
-        case "M+":
-          if (currentCalcIsNan) {
+        switch (digit) {
+          case "M+":
+            if (currentCalcIsNan) {
+              break;
+            }
+
+            setInMemory((inMemory ?? 0) + +currentCalc.calc);
+            setCurrentCalc({ ...currentCalc, calc: "" });
             break;
-          }
+          case "M-":
+            if (currentCalcIsNan) {
+              break;
+            }
 
-          setInMemory((inMemory ?? 0) + +currentCalc.calc);
-          setCurrentCalc({ ...currentCalc, calc: "" });
-          break;
-        case "M-":
-          if (currentCalcIsNan) {
+            setInMemory((inMemory ?? 0) - +currentCalc.calc);
+            setCurrentCalc({ ...currentCalc, calc: "" });
             break;
-          }
-
-          setInMemory((inMemory ?? 0) - +currentCalc.calc);
-          setCurrentCalc({ ...currentCalc, calc: "" });
-          break;
-        case "MC":
-          setInMemory(null);
-          break;
-        case "MR":
-          if (inMemory === null) {
-            setCurrentCalc({ ...currentCalc, calc: "0" });
+          case "MC":
+            setInMemory(null);
             break;
-          }
+          case "MR":
+            if (inMemory === null) {
+              setCurrentCalc({ ...currentCalc, calc: "0" });
+              break;
+            }
 
-          setCurrentCalc({ ...currentCalc, calc: inMemory.toString() });
-          break;
-        case "%":
-          debugger;
-          const match = currentCalc.calc.match(/(\d+(\.\d+)?)\s*$/);
+            setCurrentCalc({ ...currentCalc, calc: inMemory.toString() });
+            break;
+          case "%":
+            debugger;
+            const match = currentCalc.calc.match(/(\d+(\.\d+)?)\s*$/);
 
-          if (!match) {
-            return;
-          }
+            if (!match) {
+              return;
+            }
 
-          const lastNumber: string = match[1];
-          const lastChar: string =
-            currentCalc.calc[currentCalc.calc.length - 1];
+            const lastNumber: string = match[1];
+            const lastChar: string =
+              currentCalc.calc[currentCalc.calc.length - 1];
 
-          if (!/\d/.test(lastChar)) {
-            return;
-          }
+            if (!/\d/.test(lastChar)) {
+              return;
+            }
 
-          const dividedLastNumber: string = (
-            parseFloat(lastNumber) / 100
-          ).toString();
-          const outputString: string = currentCalc.calc.replace(
-            lastNumber,
-            dividedLastNumber
-          );
+            const dividedLastNumber: string = (
+              parseFloat(lastNumber) / 100
+            ).toString();
+            const outputString: string = currentCalc.calc.replace(
+              lastNumber,
+              dividedLastNumber
+            );
 
-          setCurrentCalc({ ...currentCalc, calc: outputString });
+            setCurrentCalc({ ...currentCalc, calc: outputString });
 
-          break;
-        case "C":
-          setCurrentCalc({ calc: "", result: 0 });
-          break;
-        case "=":
-          const formattedInput = currentCalc.calc
-            .replaceAll("x", "*")
-            .replaceAll("÷", "/")
-            .replace(/√(\d+(\.\d+)?)/g, "root($1)");
+            break;
+          case "C":
+            setCurrentCalc({ calc: "", result: 0 });
+            break;
+          case "=":
+            const formattedInput = currentCalc.calc
+              .replaceAll("x", "*")
+              .replaceAll("÷", "/")
+              .replace(/√(\d+(\.\d+)?)/g, "root($1)");
 
-          var lexed = mexp.lex(formattedInput, []);
-          var postfixed = mexp.toPostfix(lexed);
-          var result = mexp.postfixEval(postfixed, {});
-          setCurrentCalc({ result, calc: "" });
-          setHistory([...history, { result, calc: currentCalc.calc }]);
-          break;
+            var lexed = mexp.lex(formattedInput, []);
+            var postfixed = mexp.toPostfix(lexed);
+            var result = mexp.postfixEval(postfixed, {});
+            setCurrentCalc({ result, calc: "" });
+            setHistory([...history, { result, calc: currentCalc.calc }]);
+            break;
 
-        default:
-          if (
-            operatorsThatMustGetTheResultOnStart.includes(digit) &&
-            currentCalc.calc === "" &&
-            currentCalc.result > 0
-          ) {
-            setCurrentCalc({
-              ...currentCalc,
-              calc: currentCalc.calc + currentCalc.result + digit,
-            });
-          } else {
-            setCurrentCalc({ ...currentCalc, calc: currentCalc.calc + digit });
-          }
+          default:
+            if (
+              operatorsThatMustGetTheResultOnStart.includes(digit) &&
+              currentCalc.calc === "" &&
+              currentCalc.result > 0
+            ) {
+              setCurrentCalc({
+                ...currentCalc,
+                calc: currentCalc.calc + currentCalc.result + digit,
+              });
+            } else {
+              setCurrentCalc({
+                ...currentCalc,
+                calc: currentCalc.calc + digit,
+              });
+            }
+        }
+      } catch (error) {
+        setCurrentCalc({ calc: "An error ocurred, try again", result: 0 });
       }
     },
     [currentCalc]
